9b109ffe4f8eea2e8bc90f63fa2b4a7014759a01
delbaze <56109313+delbaze@users.noreply.github.com>
2024-07-23 16:28:50 +0200

    env

--- backend/src/lib/mongodb.ts
+++ backend/src/lib/mongodb.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb://user:password@mongo:27017/";
+// const uri = "mongodb://user:password@mongo:27017/";
+const uri = process.env.MONGODB_URI!;
 const client = new MongoClient(uri);
 
 export default client;
